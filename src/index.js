c9eef0df8d9d88aac5f27377d4562f5e6cd6f978
bonial_virto <svetlana.suslova2031@gmail.com>
2020-04-13 19:02:02 +0200

    created basic components structure with styles

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.sass';
+
+ReactDOM.render(<App />, document.getElementById('root'));
