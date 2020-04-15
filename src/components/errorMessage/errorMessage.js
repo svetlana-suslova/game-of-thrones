eb410c572c4f0dd7ba48bddd2530df1fb606b656
bonial_virto <svetlana.suslova2031@gmail.com>
2020-04-15 14:56:21 +0200

    added picture for error

--- src/components/errorMessage/errorMessage.js
+++ src/components/errorMessage/errorMessage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+
 import './errorMessage.sass';
+import img from './error.jpg';
 
 const ErrorMessage = () => {
-    return <div className="error-message">Something went wrong!</div>
+    return (
+        <>
+        <div className="error-message">Something went wrong!</div>
+        <img src={img} alt="error"/>
+        </>   
+    ) 
 }
 export default ErrorMessage;
